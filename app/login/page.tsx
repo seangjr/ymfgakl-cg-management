3b5a192fabdc005c5cac112fc5045ebbb4b09939
seangjr <sean@relampagos.org>
2024-07-11 12:59:43 +0800

    feat: prisma integration

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/pages/LoginForm";
+
+export default function Page() {
+  return (
+    <div>
+      <form>
+        <LoginForm formAction="" />
+      </form>
+    </div>
+  )
+}
